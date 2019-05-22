5d2960163dbd11cd566e0406ba923bf008f56073
Joel Severne <joelseverne@gmail.com>
2019-05-22 23:10:55 +0100

    blog page added
    blog.html created

--- mail_handler.php
+++ mail_handler.php
@@ -1,17 +1,29 @@
+<!-- <!doctype html>
+<html>
+<head>
+  <title>Response Script</title>
+</head>
+<body> -->
+
 <?php
 if(isset($_POST['submit'])){
-    $to = "joelseverne@gmail.com"; // this is your Email address
+    $to = "jseverne@hotmail.co.uk"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['firstName'];
     $last_name = $_POST['secondName'];
-    $subject = "Form submission";
-    $message = $firstName . " " . $secondName . " wrote the following:" . "\n\n" . $_POST['message'];
+    $date = $_POST['date']
+    // $subject = "Formsubmission";
+    $message = $name . " " . $lastname . " wrote the following about the following date:" . $date . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
+    $headers = "From: " . $from;
     mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // header('Location: contactTest.html');
+    // You cannot use header and echo together. It's one or the other.
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
 ?>
+<!--
+</body>
+</html> -->
