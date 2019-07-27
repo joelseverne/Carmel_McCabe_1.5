b9b5852489294e5bffb808f79ed2ce9026caed44
Joel Severne <joelseverne@gmail.com>
2019-07-27 09:11:51 +0100

    Contact page script separated into it's own folder

---
+++ JS/ContactScript.js
@@ -0,0 +1,11 @@
+var displayAlert;
+
+function AlertTrue() {
+  displayAlert = true;
+}
+
+function AlertTest() {
+  if (displayAlert = true) {
+    alert("Thanks for your message! I'll get back to you soon");
+  }
+}
