38634bf592f345c4ba17f88d6f01fbda7c8a9f50
Joel Severne <joelseverne@gmail.com>
2020-04-25 09:32:36 +0100

    Added Offer to home page
    <body onload="OfferFunction()">

---
+++ JS/ReviewDiv.js
@@ -0,0 +1,38 @@
+function OfferFunction() {
+  if (confirm("Happy New Year! - Receive 10% off all full day wedding photography bookings made throughout February.  Want to get in touch?")) {
+    window.location.href = "contactTest.html";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+}
